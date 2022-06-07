93abc7569d83a2d36fdfdc4304312bc931852919
Hailey Bates <hailey.bates001@gmail.com>
2022-06-07 17:15:16 -0400

    Fixed server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -26,7 +26,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client'));
 });
 
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+
+  startApolloServer(typeDefs, resolvers);
