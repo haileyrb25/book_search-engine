75fcc9c2a0e9fa2198eaba3d5076c761dc8c23b2
Hailey Bates <hailey.bates001@gmail.com>
2022-06-07 17:28:29 -0400

    Adding queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,24 @@
+// this will hold the query 'GET_ME' which will execute the 'me' query set up using apollo server
+
+import { gql } from '@apollo/client';
+
+export const QUERY_TECH = gql`
+  query tech {
+    tech {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_MATCHUPS = gql`
+  query matchups($_id: String) {
+    matchups(_id: $_id) {
+      _id
+      tech1
+      tech2
+      tech1_votes
+      tech2_votes
+    }
+  }
+`;
